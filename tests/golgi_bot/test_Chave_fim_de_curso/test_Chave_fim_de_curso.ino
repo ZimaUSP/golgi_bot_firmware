4e136ebf280378fdfe59a85a1d8e1c42be1ae54a
thomazitos <thomazakira@hotmail.com>
2021-10-26 09:29:28 -0300

    🚑 Teste chave fim de curso Woering properly now

--- tests/golgi_bot/test_Chave_fim_de_curso/test_Chave_fim_de_curso.ino
+++ tests/golgi_bot/test_Chave_fim_de_curso/test_Chave_fim_de_curso.ino
@@ -1,22 +1,20 @@
 #include "Chave_fim_de_curso.hpp"
 
-int chave_pin=12;
+int chave_pin=34;
 Chave_fim_de_curso *endstop;
 
 void setup() {     
   Serial.begin (9600);
-  pinMode(32,OUTPUT);
-  digitalWrite(32,HIGH);
   endstop = new Chave_fim_de_curso(chave_pin,0);
   endstop->init();
   }
    
 void loop() {
-   Serial.println(endstop->getPin());
   if (endstop->getBatente())
   {
     Serial.println("Batente");
+  }else{
+    Serial.println("sem batente");
   }
   
   }
-
