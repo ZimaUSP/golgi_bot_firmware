54faf926643140cc4e1d1145e0b53d8c93fb9995
thomazitos <thomazakira@hotmail.com>
2021-12-02 14:59:08 -0300

    🐛 State machine fixed and added ultrasonic sensor

--- tests/golgi_bot/STATE_MACHINE_MAIN/STATE_MACHINE_MAIN.ino
+++ tests/golgi_bot/STATE_MACHINE_MAIN/STATE_MACHINE_MAIN.ino
@@ -41,7 +41,7 @@ int MAX_PULSES_X=12000;
 
 // Encoder Z axis 
 #include "Encoder.hpp"
-int A_pin_Z=19; // green cable
+int A_pin_Z=19; // Green cable
 int B_pin_Z=21; // white cable
 Encoder *encoder_Z;
 int last_z_count;
@@ -66,14 +66,15 @@ Chave_fim_de_curso *endstop_R_Z;
 //PID Z axis constants
 #include "PID.hpp"
 double kp_Z = 0.1;
-double ki_Z = 0;
-double kd_Z= 1.5; 
+double ki_Z = 0.000007;
+double kd_Z= 5; 
 int output_z;
 double setPoint_z;
 PID *PID_Z; 
 
 //Count MAX
 int MAX_PULSES_Z =12000;
+
 
 
 // EIXO Y 
@@ -90,6 +91,12 @@ int Extend_pin=33; //IN4
 int Contract_pin=25; //IN3
 Atuador *Atuador_Y;
 
+// Ultrassonico
+#include "Ultrasonico.hpp"
+const int trigPin = 13;
+const int echoPin = 14;
+Ultrasonico *Ultrasonico_Y;
+
 //Serial comunication
 #include "serial_communication.hpp"
 #include "config.hpp"
@@ -98,7 +105,11 @@ Atuador *Atuador_Y;
 SerialCommunication *comu;
 
 // Estados
-char STATE = 'A' ; // 0 -> Stand by 1-> To Goal   2 -> In_Goal  3 ->To origin  
+char STATE = 0 ; // 
+#define STAND_BY 0
+#define GOING 1
+#define GETING_MEDICINE 2
+#define DROPING_MEDICINE 3
 
 
 void setup() {
@@ -136,19 +147,7 @@ void setup() {
   encoder_Z = new Encoder(A_pin_Z,B_pin_Z,1);
   encoder_Z->init();
 
-  //Set origin
-  
-
-  Set_origin_x();
-  
-  Set_origin_z();
-
-  //PID
-  PID_X = new PID(kp_x,ki_x,kd_x);
-  
-  PID_Z = new PID(kp_Z,ki_Z,kd_Z);
-
-  // Atuador
+   // Atuador
   Atuador_Y= new Atuador( Extend_pin,Contract_pin);
   Atuador_Y->init();
   Atuador_Y->Contract();
@@ -159,50 +158,71 @@ void setup() {
   Bomba_Y= new Bomba(bomba_pin);
   Bomba_Y->init();
 
+  // Ultrassonico
+  Ultrasonico_Y= new Ultrasonico(trigPin,echoPin);
+  Ultrasonico_Y->init();
+
+  //Set origin
+  
+
+  Set_origin_x();
+  
+  Set_origin_z();
+
+  Set_max_x();
+  
+  Set_max_z();
+
+  Set_origin_x();
+  
+  Set_origin_z();
+
+  //PID
+  PID_X = new PID(kp_x,ki_x,kd_x);
+  
+  PID_Z = new PID(kp_Z,ki_Z,kd_Z);
+
+ 
+
 }
 
 void loop() {
   switch(STATE) {
-      case 'A' :
+      case STAND_BY :
         Serial.println("STAND-BY");
         // Recive Set point
         read_setpoint();
-        break;
-      case 'B' :
+        return;
+      case GOING :
         
         // PID X
         output_x = PID_X->computePID(encoder_X->getPulses(),setPoint_x);
-
+        // Setting direction of motion acording to output_x PID
+        move_X(); 
         // PID Z
         output_z = PID_Z->computePID(encoder_Z->getPulses(),setPoint_z);
-
-        // Setting direction of motion acording to output_x PID
-        move_X();
-
         // Setting direction of motion acording to output_z PID
         move_Z();
 
-        Serial.println("GOING");
-        if((encoder_X->getPulses()==last_x_count) && (encoder_Z->getPulses()==last_z_count )){
-          STATE='C';
-          Serial.print("counter X :");
-          Serial.println(encoder_X->getPulses());
-          Serial.print("    counter Z :");
-          Serial.println(encoder_Z->getPulses());
-        }
+        //Code does not work without this delay (?)
+        delay(2);
+        
+        check_position();
+        
         last_x_count=encoder_X->getPulses();
         last_z_count=encoder_Z->getPulses();
-        break;
-      case 'C' :
-        Serial.println("GET-MEDIICNE");
+        return;
+      case GETING_MEDICINE :
+
+        Serial.println("GETING_MEDICINE");
         Get_medicine();
-        break;
-      case 'D' :
-        Serial.println("DROP-MEDICNE");
+        return;
+      case DROPING_MEDICINE :
+        Serial.println("DROPING_MEDICINE");
         Set_origin_x();
-        Set_origin_z();
+        Set_max_z();
         Drop_medicine();
-        break;
+        return;
    }
     
     // Debug print X
@@ -242,16 +262,46 @@ void Set_origin_z(){
   BTS_Z->SetPWM_R(0);
 }
 
+void Set_max_x(){
+  while (digitalRead(chave_L_X)==HIGH){
+    BTS_X->Set_L(100);
+  }
+  MAX_PULSES_X = encoder_X->getPulses();
+  BTS_X->SetPWM_L(0);
+}
+
+void Set_max_z(){
+  while (digitalRead(chave_L_Z)==HIGH){
+    BTS_Z->Set_L(100);
+  }
+  MAX_PULSES_Z = encoder_Z->getPulses();
+  BTS_Z->SetPWM_L(0);
+}
+
+
 void read_setpoint(){
     if(Serial.available()){
-      STATE='B';
+      
+      STATE=GOING;
+
       char *ptr;
       comu->read_data();
       char* recived=string_to_char(comu->get_received_data());
+
       ptr = strtok(recived, "-");
+      if (atoi(string_to_char(ptr))>MAX_PULSES_X){
+       setPoint_x=MAX_PULSES_X ;
+      }else{
       setPoint_x=atoi(string_to_char(ptr)); 
+      }
       ptr = strtok (NULL, "-");
+      if (atoi(string_to_char(ptr))>MAX_PULSES_Z){
+       setPoint_z=MAX_PULSES_Z;
+      }else{
       setPoint_z=atoi(string_to_char(ptr));
+      }
+      PID_X->reset();
+      PID_Z->reset();
       last_x_count=-5000;
       last_z_count=-5000;
     }
@@ -294,12 +344,17 @@ void Get_medicine(){
   Atuador_Y->Contract();
   delay(1000);
   Atuador_Y->Stop();
-  STATE='D';
+  if(Ultrasonico_Y->get_distance()<10){
+    Serial.println(Ultrasonico_Y->get_distance());
+    STATE=DROPING_MEDICINE;
+
+    }
+
 }
 
 void Drop_medicine(){
   Bomba_Y->turn_off();
-  STATE='A';
+  STATE=STAND_BY;
 }
 
 
@@ -307,7 +362,7 @@ void check_position(){
   if((encoder_X->getPulses()==last_x_count) && (encoder_Z->getPulses()==last_z_count )){
           BTS_X->SetPWM_R(0);
           BTS_Z->SetPWM_R(0);
-          STATE='C';
+          STATE=GETING_MEDICINE;
           Serial.print("counter X :");
           Serial.println(encoder_X->getPulses());
           Serial.print("    counter Z :");
@@ -315,3 +370,4 @@ void check_position(){
           return;
         }
 }
+
