9bb7cea8bb95c475ddfc6cb34451b9d71182a26d
“gunogueiras” <g.nogueiras@usp.br>
2022-08-19 17:31:43 -0300

    ✅ Update Cable Chain

--- tests/golgi_bot/test_components/test_CABLE_CHAIN_Z/test_CABLE_CHAIN_Z.ino
+++ tests/golgi_bot/test_components/test_CABLE_CHAIN_Z/test_CABLE_CHAIN_Z.ino
@@ -1,19 +1,12 @@
 // BTS Z axis 
 #include "H_bridge_controller.hpp"
-int R_pin_Z=17; // R bts
-int L_pin_Z=18; // L bts
-int PWM_frequency = 40000;
-int PWM_resolution = 8;
-int R_channel_Z=1;
+#include "config.hpp"
+int R_channel_Z=1; // Na config.hpp o R_channel_Z=2 
 int L_channel_Z=2;
-int PWM_R_Z=0;
-int PWM_L_Z=0;
 H_bridge_controller *BTS_Z;
 
 // Chave fim de curso Z axis
-#include "Chave_fim_de_curso.hpp"
-int chave_L_Z=34; 
-int chave_R_Z=35; 
+#include "Chave_fim_de_curso.hpp" 
 Chave_fim_de_curso *endstop_L_Z; 
 Chave_fim_de_curso *endstop_R_Z; 
 
@@ -29,9 +22,8 @@ void setup() {
   endstop_R_Z->init();
 
   // BTS
-  BTS_Z= new H_bridge_controller( R_pin_Z, L_pin_Z, PWM_frequency, PWM_resolution, R_channel_Z, L_channel_Z);
+  BTS_Z= new H_bridge_controller(R_pin_Z, L_pin_Z, PWM_frequency_channel, PWM_resolution_channel, R_channel_Z, L_channel_Z);
   BTS_Z->init();
-
 
 }
 
